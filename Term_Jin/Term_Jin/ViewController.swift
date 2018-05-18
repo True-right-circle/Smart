a02d8928d5a7262b3fcc69458e2a98feeb3f584d
woowon123 <wlsdndnjs12@naver.com>
2018-05-18 22:25:55 +0900

    base
    base

---
+++ Term_Jin/Term_Jin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Term_Jin
+//
+//  Created by Jin on 2018. 5. 18..
+//  Copyright © 2018년 Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
