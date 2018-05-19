34cf496e9fe15ad16585359342bcd3b944f0c70a
woowon123 <wlsdndnjs12@naver.com>
2018-05-19 22:07:32 +0900

    add imageview add button
    add imageview add button

--- Term_Jin/Term_Jin/ViewController.swift
+++ Term_Jin/Term_Jin/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LogoImage: UIImageView!
+    @IBOutlet weak var assingnButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
